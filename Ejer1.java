ceb9a9f36316296c67747272493c9a1cb80bf877
Auto Logon <autologon@CSED.LAB>
2024-03-14 09:23:17 -0300

    vercion 1

---
+++ Ejer1.java
@@ -0,0 +1,23 @@
+  import java.util.Scanner;
+
+  public class Ejer1 {
+    
+    public static void executar () {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite um número: ");
+        double numero = scanner.nextDouble();
+        
+        if (numero > 10) {
+            System.out.println("Número maior do que 10!");
+        } else {
+            System.out.println("Número menor ou igual a 10!");
+        }
+        
+        // Fecha o scanner
+        scanner.close();
+    }
+}
+
+
